45f958ec93de9f16a40a335797b00091d3e4e0e7
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-05-21 23:19:57 +0200

    Add basic content for Search component
    This component will be responsible for updating  the name of the
    currently searched pokemon

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import data from "./pokemonData";
 import PokemonList from "./components/pokemonList/PokemonList";
+import Search from "./components/search/Search";
 
 function App() {
   const [ pokemonList, setPokemonList ] = React.useState(data);
   return (
     <div className="App">
+      <Search />
       <PokemonList pokemonData={pokemonList} />
     </div>
   );
---
+++ src/components/search/Search.js
@@ -0,0 +1,10 @@
+export default function Search() {
+  return (
+    <form className="searcher">
+      <label htmlFor="search" className="searcher__description">
+        Wpisz nazwe pokemona
+      </label>
+      <input className="search__input" type="text" id="search" />
+    </form>
+  );
+}
