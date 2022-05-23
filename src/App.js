14e8a3d8c517d40028b7c8d056715884dd060c0e
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-05-23 22:09:58 +0200

    Create state related with Search component
    currentTypedPokemonName will be update by Search component, and it will
    be passed to PokemonList to filter only matches pokemon name

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Search from "./components/search/Search";
 
 function App() {
   const [ pokemonList, setPokemonList ] = React.useState(data);
+
+  //This state + function are passed to Search component - Search component updates the currentTypedPokemonName, and based on this in PokemonList the pokemonList is filtered and finally we see only pokemons which match to the typed name
+  const [
+    currentTypedPokemonName,
+    setCurrentTypedPokemonName,
+  ] = React.useState("");
+
+  function updatePokemonName(name) {
+    setCurrentTypedPokemonName(name);
+  }
+
   return (
     <div className="App">
       <Search />
