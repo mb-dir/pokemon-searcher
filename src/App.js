88f2502eead129285bac7bc9435316af7b9107f4
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-05-25 22:06:47 +0200

    Delete hard-coded data
    This hard-coded data was usefull for an example. Now I am going to
    implement the logic based on data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import data from "./pokemonData";
 import PokemonList from "./components/pokemonList/PokemonList";
 import Search from "./components/search/Search";
 
 function App() {
-  const [ pokemonList, setPokemonList ] = React.useState(data);
+  const [ pokemonList, setPokemonList ] = React.useState([]);
 
   //This state + function are passed to Search component - Search component updates the currentTypedPokemonName, and based on this in PokemonList the pokemonList is filtered and finally we see only pokemons which match to the typed name
   const [
