243e1338055f64cd7a63cb7014bf3ddfd37f589a
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-12-14 21:44:50 +0100

    Use pokemon-list hook

--- src/App.js
+++ src/App.js
@@ -1,108 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import { PokemonList } from "./components/pokemonList/PokemonList";
 import { Search } from "./components/search/Search";
 import { Pokedex } from "./components/pokedex/Pokedex";
-import { setItemToStorage, getItemFromStorage } from "./helpers/storage";
+import { usePokemonList } from "./hooks/use-pokemon-list";
 
 function App() {
-  const [ pokemonList, setPokemonList ] = useState([]);
-  const [ requestStatus, setRequestStatus ] = useState("pending");
-
   const [ currentTypedPokemonName, setCurrentTypedPokemonName ] = useState("");
+  const {
+    pokemonList,
+    requestStatus,
+    pokedexList,
+    deleteFromPokedex,
+    addToPokedex,
+  } = usePokemonList();
 
   function updatePokemonName(name) {
     setCurrentTypedPokemonName(name);
   }
 
-  function createPokemonPromise(pokemonUrl) {
-    return new Promise((resolve, reject) => {
-      fetch(pokemonUrl)
-        .then(res => res.json())
-        .then(res => resolve(res))
-        .catch(err => reject.log(err));
-    });
-  }
-
-  useEffect(() => {
-    const pokemonsPromisesArray = [];
-    const pokemonsProperInfo = [];
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=100")
-      .then(res => res.json())
-      .then(res => {
-        res.results.forEach(el => {
-          const pokemonPromise = createPokemonPromise(el.url);
-          pokemonsPromisesArray.push(pokemonPromise);
-        });
-        return pokemonsPromisesArray;
-      })
-      .then(res => {
-        (async function getPokemonInfo() {
-          for (const pokemon of await Promise.all(res)) {
-            const {
-              abilities,
-              name,
-              sprites,
-              types,
-              base_experience,
-              height,
-              weight,
-            } = pokemon;
-            const pokemonObj = {
-              pokemonName: name,
-              pokemonAbilities: abilities,
-              pokemonTypes: types,
-              pokemonImg: sprites.front_default,
-              pokemonBaseExperience: base_experience,
-              pokemonHeight: height,
-              pokemonWeight: weight,
-            };
-            pokemonsProperInfo.push(pokemonObj);
-          }
-          setPokemonList(pokemonsProperInfo);
-          setRequestStatus("resovled");
-        })();
-      })
-      .catch(() => {
-        setRequestStatus("rejected");
-      });
-  }, []);
-
   const [ isPokedexOpen, setIsPokedexOpen ] = useState(false);
   function togglePokedex() {
     setIsPokedexOpen(prev => !prev);
   }
-  const [ pokedexList, setPokedexList ] = useState(() => {
-    return JSON.parse(getItemFromStorage("pokedex")) || [];
-  });
-  function addToPokedex(pokemon) {
-    setPokedexList(prevList => {
-      const isPokemonAlreadyExist = prevList.some(element => {
-        if (element.pokemonName === pokemon.pokemonName) {
-          return true;
-        }
-        return false;
-      });
-      if (isPokemonAlreadyExist) {
-        return [ ...prevList ];
-      } else {
-        return [ ...prevList, pokemon ];
-      }
-    });
-  }
-  function deleteFromPokedex(pokemon) {
-    setPokedexList(prevList => {
-      return prevList.filter(el => {
-        return el.pokemonName !== pokemon.pokemonName;
-      });
-    });
-  }
-  useEffect(
-    () => {
-      setItemToStorage("pokedex", JSON.stringify(pokedexList));
-    },
-    [ pokedexList ]
-  );
 
   return (
     <div className="App">
