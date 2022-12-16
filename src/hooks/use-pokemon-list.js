5ca54b2fb306576f34c1aff186b436922b52d490
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-12-16 22:41:54 +0100

    Rewrite promises to async/await

--- src/hooks/use-pokemon-list.js
+++ src/hooks/use-pokemon-list.js
@@ -4,57 +4,70 @@ export const usePokemonList = () => {
   const [ pokemonList, setPokemonList ] = useState([]);
   const [ requestStatus, setRequestStatus ] = useState("pending");
 
-  const createPokemonPromise = pokemonUrl => {
-    return new Promise((resolve, reject) => {
-      fetch(pokemonUrl)
-        .then(res => res.json())
-        .then(res => resolve(res))
-        .catch(err => reject.log(err));
+  function createPokemonPromise(pokemonUrl) {
+    return new Promise(async resolve => {
+      try {
+        const pokemonPromise = await fetch(pokemonUrl);
+        const pokemonPromiseResponse = await pokemonPromise.json();
+        resolve(pokemonPromiseResponse);
+      } catch (e) {
+        setRequestStatus("rejected");
+      }
     });
-  };
+  }
 
   useEffect(() => {
-    const pokemonsPromisesArray = [];
-    const pokemonsProperInfo = [];
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=100")
-      .then(res => res.json())
-      .then(res => {
-        res.results.forEach(el => {
-          const pokemonPromise = createPokemonPromise(el.url);
+    async function getPokemonPromises() {
+      try {
+        const pokemonsPromisesArray = [];
+
+        const pokemons = await fetch(
+          "https://pokeapi.co/api/v2/pokemon/?limit=100"
+        );
+        const pokemonsResponse = await pokemons.json();
+
+        await pokemonsResponse.results.forEach(pokemon => {
+          const pokemonPromise = createPokemonPromise(pokemon.url);
           pokemonsPromisesArray.push(pokemonPromise);
         });
+
         return pokemonsPromisesArray;
-      })
-      .then(res => {
-        (async function getPokemonInfo() {
-          for (const pokemon of await Promise.all(res)) {
-            const {
-              abilities,
-              name,
-              sprites,
-              types,
-              base_experience,
-              height,
-              weight,
-            } = pokemon;
-            const pokemonObj = {
-              pokemonName: name,
-              pokemonAbilities: abilities,
-              pokemonTypes: types,
-              pokemonImg: sprites.front_default,
-              pokemonBaseExperience: base_experience,
-              pokemonHeight: height,
-              pokemonWeight: weight,
-            };
-            pokemonsProperInfo.push(pokemonObj);
-          }
-          setPokemonList(pokemonsProperInfo);
-          setRequestStatus("resovled");
-        })();
-      })
-      .catch(() => {
+      } catch (e) {
         setRequestStatus("rejected");
-      });
+      }
+    }
+
+    (async function getPokemonInfo() {
+      try {
+        const pokemonsProperInfo = [];
+        for await (const pokemon of await getPokemonPromises()) {
+          const {
+            abilities,
+            name,
+            sprites,
+            types,
+            base_experience,
+            height,
+            weight,
+          } = pokemon;
+          const pokemonObj = {
+            pokemonName: name,
+            pokemonAbilities: abilities,
+            pokemonTypes: types,
+            pokemonImg: sprites.front_default,
+            pokemonBaseExperience: base_experience,
+            pokemonHeight: height,
+            pokemonWeight: weight,
+          };
+
+          pokemonsProperInfo.push(pokemonObj);
+        }
+        setPokemonList(pokemonsProperInfo);
+        setRequestStatus("resovled");
+      } catch (e) {
+        setRequestStatus("rejected");
+      }
+    })();
   }, []);
 
   return {
