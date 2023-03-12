92971ac042505bf37c767bcaee91c381cbdaa148
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-03-12 23:26:55 +0100

    Add enums and types

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type singlePokemon = {
+  pokemonName: string;
+  pokemonAbilities: pokemonAbilities[];
+  pokemonTypes: pokemonTypes[];
+  pokemonImg: string;
+  pokemonBaseExperience: string;
+  pokemonHeight: string;
+  pokemonWeight: string;
+};
+
+export type pokemonTypes = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+export type pokemonAbilities = {
+  ability: { name: string; url: string };
+  is_hidden: boolean;
+  slot: number;
+};
